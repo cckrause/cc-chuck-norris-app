dcfab5d9933a93841813b57583aec6f162950dfd
Christian <chris@christiankrause.de>
2024-02-29 09:10:18 +0100

    adding axios and implementing categories request

---
+++ api/norris.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import { QueryFunction } from '@tanstack/react-query'
+
+const API_BASE_URL = 'https://api.chucknorris.io'
+
+export type NorrisJoke = {
+    id: string
+    categories: NorrisCategories
+    created_at: string
+    icon_url: string
+    updated_at: string
+    url: string
+    value: string
+}
+
+export type NorrisCategories = string[]
+
+export const getCategories: QueryFunction<NorrisCategories> = async ({
+    signal,
+}) => {
+    const res = await axios.get<NorrisCategories>(
+        `${API_BASE_URL}/jokes/categories`,
+        { signal } // pass in signal to enable aborting requests on refetch
+    )
+    return res.data
+}
