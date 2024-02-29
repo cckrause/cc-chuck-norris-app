c2e4f961be090880336efeae037d1fc697549888
Christian <chris@christiankrause.de>
2024-02-29 08:59:05 +0100

    implements home page and basic layouting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,20 @@
-import "../styles/globals.css";
+import '../styles/globals.css'
+import { Container, Heading, Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return (
+        <Theme appearance="dark" accentColor="blue" radius="full">
+            <Container size="2" p="4">
+                <Heading my="6" size="8">
+                    Chuck Norris is roundhouse kickin' jokes in your face! 🧔
+                </Heading>
+                <QueryClientProvider client={queryClient}>
+                    <Component {...pageProps} />
+                </QueryClientProvider>
+            </Container>
+        </Theme>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-    return 'hello world'
+import { Button, Flex, Text } from '@radix-ui/themes'
+import { useRouter } from 'next/router'
+
+const Home = () => {
+    const router = useRouter()
+
+    return (
+        <Flex direction="column" justify="center" gap="4">
+            <Button
+                variant="soft"
+                color="blue"
+                onClick={() => router.push(`/jokes/`)}
+            >
+                Random Joke
+            </Button>
+            <Text>TODO: Categories</Text>
+        </Flex>
+    )
 }
+
+export default Home
